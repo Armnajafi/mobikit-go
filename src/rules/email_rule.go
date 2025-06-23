09db2e79b03979cc6b0d262726b04f3e04d61552
AmirMohammad Najafi <73462542+Armnajafi@users.noreply.github.com>
2025-06-23 23:26:30 +0330

    feat: add email rule to rules

---
+++ src/rules/email_rule.go
@@ -0,0 +1,19 @@
+package rules
+
+import (
+	"regexp"
+)
+
+type EmailRule struct {
+	Message string
+}
+
+func (r EmailRule) Validate(value interface{}) (bool, string) {
+	if v, ok := value.(string); ok {
+		re := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$`)
+		if !re.MatchString(v) {
+			return false, r.Message
+		}
+	}
+	return true, ""
+}

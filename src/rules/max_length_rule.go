950d8f856bacd36301a05580d1345586350a9ff2
AmirMohammad Najafi <73462542+Armnajafi@users.noreply.github.com>
2025-06-23 23:20:08 +0330

    feat: add min length rule  and change max length rule

--- src/rules/max_length_rule.go
+++ src/rules/max_length_rule.go
@@ -1,13 +1,13 @@
 package rules
 
-type MinLengthRule struct {
-	Min     int
+type MaxLengthRule struct {
+	Max     int
 	Message string
 }
 
-func (r MinLengthRule) Validate(value interface{}) (bool, string) {
+func (r MaxLengthRule) Validate(value interface{}) (bool, string) {
 	if v, ok := value.(string); ok {
-		if len(v) < r.Min {
+		if len(v) < r.Max {
 			return false, r.Message
 		}
 	}
